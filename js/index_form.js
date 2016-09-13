6e3e5e8c478d9badbe65e19a39062dc4a91587ce
davmstuff <Edem.Enosch.Fiawoo@usherbrooke.ca>
2016-09-13 02:21:16 -0400

    Changing background Adding iimage preview before upload

--- js/index_form.js
+++ js/index_form.js
@@ -65,7 +65,8 @@ $(".previous").click(function(){
 			//3. increase opacity of previous_fs to 1 as it moves in
 			opacity = 1 - now;
 			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity , 'position':'absolute'});
+			
 		},
 		duration: 800,
 		complete: function(){
